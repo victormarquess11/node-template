52d0d07791c1795de6f68b8b32c145dd8982a3e1
victormarquess11 <victormarquess11@gmail.com>
2024-12-28 12:04:30 -0300

    add: initial project setup with Express, TypeScript, ESLint and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    {
+        ignores: ["**/*.js"],
+    },
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+);
